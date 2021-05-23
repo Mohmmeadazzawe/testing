23a825c472d75a553a1493a2bbb58504ad2f0faa
Mohmmead.az <mohmmead.w.azzawe@gmail.com>
2021-05-23 18:27:16 +0300

    hello

---
+++ android/app/src/main/kotlin/com/coachazzawe/malkanat/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.coachazzawe.malkanat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Splash Screen',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Splash2(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+class Splash2 extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 6,
+      navigateAfterSeconds: new SecondScreen(),
+      title: new Text('GeeksForGeeks',textScaleFactor: 2,),
+      image: new Image.asset(r'images/logo22.png'),
+      loadingText: Text("Loading"),
+      photoSize: 100.0,
+      loaderColor: Colors.amber.shade400,
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:Text("GeeksForGeeks")),
+      body: Center(
+          child:Text("Home page",textScaleFactor: 2,)
+      ),
+    );
+  }
+}
